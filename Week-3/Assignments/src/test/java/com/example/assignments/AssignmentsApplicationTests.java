4ce17d38e82793af1ba6ac848dc592a8f8b7cdaf
Jackchiang1010 <jackgg44109@gmail.com>
2024-07-01 11:02:57 +0800

    commit Week3-Assignment1

---
+++ Week-3/Assignments/src/test/java/com/example/assignments/AssignmentsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.assignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+class AssignmentsApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentsApplicationTests.class, args);
+
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> serverCustomizer() {
+		return server -> server.setPort(3000);
+	}
+}
