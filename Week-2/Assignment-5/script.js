ae8332b5ecefd1f377df13af13e79128d3d2af18
Jackchiang1010 <jackgg44109@gmail.com>
2024-07-02 09:35:40 +0800

    commit Week2-Assignment5 binarySearch modified

--- Week-2/Assignment-5/script.js
+++ Week-2/Assignment-5/script.js
@@ -1,34 +1,50 @@
 function binarySearchPosition(numbers, target) {
     // your code here
-    let indexCenter = Math.floor(numbers.length / 2);
+    let indexCenter;
     let indexSmall = 0;
     let indexBig = numbers.length -1;
 
-    for( let i = 0; i <= Math.floor(numbers.length / 2); i++ ) {
-        if( numbers[indexCenter] === target ) {
+    // for( let i = 0; i <= Math.floor(numbers.length / 2); i++ ) {
+    //     if( numbers[indexCenter] === target ) {
+            
+    //         return indexCenter;
 
-            //console.log(`indexCenterIF: ${indexCenter}`);
-            return indexCenter;
+    //     } else if( numbers[indexCenter] > target ) {
 
-        } else if( numbers[indexCenter] > target ) {
+    //         indexBig = indexCenter;
+    //         indexCenter = Math.floor(indexCenter / 2);
 
-            indexBig = indexCenter;
-            indexCenter = Math.floor(indexCenter / 2);
-            //console.log(`indexBig: ${indexBig}`);
-            //console.log(`indexCenter: ${indexCenter}`);
+    //     } else if( numbers[indexCenter] < target ){
+
+    //         indexSmall = indexCenter;
+    //         indexCenter = Math.floor( ( indexSmall + indexBig ) / 2);
+
+    //     } else {
+
+    //         console.log('error');
+    //         return -1;
+
+    //     }
+    // }
+
+    while( indexSmall <= indexBig ) {
+
+        indexCenter = Math.floor( ( indexSmall + indexBig ) / 2);
+
+        if( numbers[indexCenter] > target ) {
+
+            indexBig = indexCenter - 1;
 
         } else if( numbers[indexCenter] < target ){
 
-            indexSmall = indexCenter;
-            indexCenter = Math.floor( ( indexSmall + indexBig ) / 2);
-            //console.log(`indexSmall: ${indexSmall}`);
+            indexSmall = indexCenter + 1;
 
-        } else {
+        } else if(numbers[indexCenter] === target) {
 
-            console.log('error');
-            return -1;
+            return indexCenter;
 
-        }
+        } 
+
     }
 }
 console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); // should print 0
