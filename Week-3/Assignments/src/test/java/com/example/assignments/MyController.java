386b5ec8133fb7e1b11ecfdb8b082ca213d6c2aa
Jackchiang1010 <jackgg44109@gmail.com>
2024-07-03 14:25:54 +0800

    commit Week3-Assignment2

--- Week-3/Assignments/src/test/java/com/example/assignments/MyController.java
+++ Week-3/Assignments/src/test/java/com/example/assignments/MyController.java
@@ -1,14 +1,54 @@
 package com.example.assignments;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyController {
+
+    //Assignment1
+
+    //http://localhost:3000/
 
     @RequestMapping("/")
     public String test() {
         System.out.println("Hi!");
         return "Hello, My Server!";
     }
+
+    //Assignment2
+
+    //http://localhost:3000/data
+    //http://localhost:3000/data?number=5.5
+    //http://localhost:3000/data?number=5
+    //http://localhost:3000/data?number=100000000
+
+    //When number is vary large ,web site will no change.
+    //I think sum is out of datatype's range.
+    //because I tried change sum's datatype to long , it is work.
+
+    @RequestMapping(value ="/data", method = RequestMethod.GET)
+    public String sum(@RequestParam(required = false) Float number) {
+        int sum = 0;
+
+        if( number == null ) {
+
+            return "Lack of Parameter";
+
+        } else if( number - Math.floor(number) > 0 ) {
+
+            return "Wrong Parameter";
+
+        } else {
+
+            for (int i = 0; i <= number; i++) {
+                sum += i;
+            }
+
+            System.out.printf("sum = %s", sum);
+            return String.valueOf(sum);
+
+        }
+    }
+
+
 }
