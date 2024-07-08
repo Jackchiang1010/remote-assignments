fcb3baf72ee86514e86df5e236929d9993836af7
Jackchiang1010 <jackgg44109@gmail.com>
2024-07-08 13:38:09 +0800

    commit Week3-Assignment4 bug for URL

--- Week-3/Assignments/src/main/java/com/example/assignments/Assignment4.java
+++ Week-3/Assignments/src/main/java/com/example/assignments/Assignment4.java
@@ -15,41 +15,56 @@ public class Assignment4 {
     //http://localhost:3000/setCookie
     //http://localhost:3000/deleteCookie
 
-    @RequestMapping(value = "/setCookie", method = RequestMethod.GET)
-    public String setCookie(HttpServletResponse response) {
-
-        // 建立一個Cookie 物件
-        Cookie cookie = new Cookie("username", "kaijun");
-
-        // 設置過期時間，若無設置時間，其生命週期將持續到Session 過期為止
-        cookie.setMaxAge(7*24*60*60);
-
-        // 設置HTTPS 安全的Cookie
-        cookie.setSecure(true);
-
-        // 將Cookie 物件加入Response 中
-        response.addCookie(cookie);
-
-        return "redirect:http://localhost:3000/myName.html";
-    }
-
-    @RequestMapping(value = "/deleteCookie", method = RequestMethod.GET)
-    public String deleteCookie(HttpServletResponse response) {
-
-        // 將Cookie 值設置為null
-        Cookie cookie = new Cookie("username", null);
-
-        // 設置過期時間為0
-        cookie.setMaxAge(0);
-
-        // 將Cookie 物件加入Response 中
-        response.addCookie(cookie);
-
-        return "redirect:http://localhost:3000/myName.html";
-    }
+//    @RequestMapping(value = "/setCookie", method = RequestMethod.GET)
+//    public String setCookie(HttpServletResponse response) {
+//
+//        // create Cookie object
+//        Cookie cookie = new Cookie("username", "kaijun");
+//
+//        // set overtime，若無設置時間，其生命週期將持續到Session 過期為止
+//        cookie.setMaxAge(7*24*60*60);
+//
+//        // set HTTPS safe Cookie
+//        cookie.setSecure(true);
+//
+//        // Add Cookie object to Response
+//        response.addCookie(cookie);
+//
+//        return "myName";
+//    }
+//
+//    @RequestMapping(value = "/deleteCookie", method = RequestMethod.GET)
+//    public String deleteCookie(HttpServletResponse response) {
+//
+//        // set Cookie = null
+//        Cookie cookie = new Cookie("username", null);
+//
+//        // set overtime = 0
+//        cookie.setMaxAge(0);
+//
+//        // Add Cookie object to Response
+//        response.addCookie(cookie);
+//
+//        return "myName";
+//    }
 
     @RequestMapping(value = "/myName", method = RequestMethod.GET)
     public String getCookie(@CookieValue(value = "username", required = false) String name, HttpServletResponse hsr) {
+
+        if( name == null ) {
+
+//            return "redirect:http://localhost:3000/trackName.html";
+            return "trackName";
+
+        } else {
+
+            return "myName";
+
+        }
+    }
+
+    @RequestMapping(value = "/trackName", method = RequestMethod.GET)
+    public String setCookie(@CookieValue(value = "username", required = false) String name, HttpServletResponse hsr) {
 
 //        Cookie cookie = new Cookie("username", "test123");
 //        cookie.setMaxAge(60*60);
@@ -58,32 +73,11 @@ public class Assignment4 {
 
         if( name == null ) {
 
-            return "redirect:http://localhost:3000/trackName.html";
+            return "trackName";
 
         } else {
 
-//            System.out.println(cookie);
-            return "redirect:http://localhost:3000/myName.html";
-
-        }
-    }
-
-    @RequestMapping(value = "/trackName", method = RequestMethod.GET)
-    public String setCookie(@CookieValue(value = "username", required = false) String name, HttpServletResponse hsr) {
-
-        Cookie cookie = new Cookie("username", "test123");
-        cookie.setMaxAge(60*60);
-        cookie.setPath("/");
-        hsr.addCookie(cookie);
-
-        if( name == null ) {
-
-            return "redirect:http://localhost:3000/trackName.html";
-
-        } else {
-
-//            System.out.println(cookie);
-            return "redirect:http://localhost:3000/myName.html";
+            return "myName";
 
         }
     }
